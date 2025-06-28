04f506ba6c5787cc33fb1f4fd70d97820baaee16
Khaled Maged <75185708+Maged-10@users.noreply.github.com>
2025-06-28 22:40:35 +0300

    ok

--- api/main.py
+++ api/main.py
@@ -1,27 +1,30 @@
-import asyncio
-from os import getenv
-
+import os
+from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import Update
 from aiogram.filters import Command
 from aiogram.types import Message
 
-TOKEN = getenv("TELEGRAM_BOT_TOKEN")
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+bot = Bot(token=TOKEN)
+dp = Dispatcher(storage=MemoryStorage())
+app = FastAPI()
 
-dp = Dispatcher()
 
-
-# Command handler
 @dp.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
-    await message.answer("Hello! I'm a bot created with aiogram.")
+    await message.answer("Hello! I'm a bot created with aiogram on Vercel.")
 
 
-# Run the bot
-async def main() -> None:
-    bot = Bot(token=TOKEN)
-    await dp.start_polling(bot)
+@app.post("webhook")
+async def telegram_webhook(req: Request):
+    data = await req.json()
+    update = Update.model_validate(data)
+    await dp.feed_update(bot, update)
+    return {"ok": True}
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
-          
+@app.on_event("startup")
+async def on_startup():
+    await bot.set_webhook("https://test-telegram-fawn.vercel.app/webhook")
